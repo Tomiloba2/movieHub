7e06d07445800580d23c43d8184c5e9350595b5b
TOMILOBA <oluwatomilobaomojola@gmail.com>
2023-08-16 23:51:15 -0700

    fixed error in the genre page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,7 @@
 import { useSearch } from '../context/SearchContext';
 import { LoadingSpinner } from '../components/Loading';
 import { Error } from '../components/Error';
-import fetchData, { apiKey, imgUrl } from '../libs/Async';
+import { imgUrl } from '../libs/Async';
 import { Link } from 'react-router-dom';
 import { SearchBar } from '../components/SearchBar';
 import notFound from '../assets/not-found.png';
@@ -22,48 +22,48 @@ export function SearchPage() {
             <div>
                 <SearchBar />
             </div>
-            {data?.length===0?(
+            {data?.length === 0 ? (
                 <div className="genres-css">
-                    {genres.map((item)=>{
-                        return(
-                            <section  key={item.id}>
+                    {genres.map((item) => {
+                        return (
+                            <section key={item.id}>
                                 <Link to={`/genres/${item.id}`}>
-                                <img src={item.img} alt={item.name} />
-                                <p>
-                                    {item.name}
-                                </p>
+                                    <img src={item.img} alt={item.name} />
+                                    <p>
+                                        {item.name}
+                                    </p>
                                 </Link>
                             </section>
                         )
                     })}
                 </div>
-            ):(
+            ) : (
                 <div className='search'>
-                <section className='top-result'>
-                    <h3>Top Results</h3>
-                    <article>
-                        {data?.map((item) => {
-                            return (
-                                <div key={item.id}>
-                                    <img loading='lazy' src={item.backdrop_path!==null?`${imgUrl}${item.backdrop_path}`:notFound} alt={item.original_title} />
-                                    <p>
-                                        <Link to={`/details/${item.id}`} style={{
-                                            color:`white`,
-                                            textDecoration:`none`
-                                        }}>
-                                            {item.original_title}
-                                            <br />
-                                            {item.media_type} {item.release_date}
-                                        </Link>
-                                    </p>
-                                </div>
-                            )
-                        })}
-                    </article>
-                </section>
-            </div>
+                    <section className='top-result'>
+                        <h3>Top Results</h3>
+                        <article>
+                            {data?.map((item) => {
+                                return (
+                                    <div key={item.id}>
+                                        <img loading='lazy' src={item.backdrop_path !== null ? `${imgUrl}${item.backdrop_path}` : notFound} alt={item.original_title} />
+                                        <p>
+                                            <Link to={`/details/${item.id}`} style={{
+                                                color: `white`,
+                                                textDecoration: `none`
+                                            }}>
+                                                {item.original_title}
+                                                <br />
+                                                {item.media_type} {item.release_date}
+                                            </Link>
+                                        </p>
+                                    </div>
+                                )
+                            })}
+                        </article>
+                    </section>
+                </div>
             )}
-            
+
         </>
     );
 }
